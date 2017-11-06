db1d108ea56d850b2bc037049369488e150687b2
johnkariungi <john.k.gitonga@gmail.com>
2017-11-05 16:25:16 -0800

    Initial commit

--- test/java/com/linkedin/learning/rest/ReservationResource.java
+++ test/java/com/linkedin/learning/rest/ReservationResource.java
@@ -15,6 +15,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = LinkedInLearningFullStackAppAngularSpringBootApplication.class,
 webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
 public class ReservationResource {
 
     @LocalServerPort
